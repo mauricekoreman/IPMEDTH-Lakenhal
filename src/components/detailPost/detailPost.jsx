8e703ac602e10f699e2b9f36133a649e3835c4c3
samirtown <samirf4thi@outlook.com>
2021-06-03 21:45:07 +0200

    details post front end

--- src/components/detailPost/detailPost.jsx
+++ src/components/detailPost/detailPost.jsx
@@ -1,28 +1,79 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
-import Slide from '@material-ui/core/Slide';
-import { useState } from "react";
+import Image from "material-ui-image";
+import lakenhal_sw from "../../assets/img/lakenhal_sw.png";
+import pf from "../../assets/img/placeholders/profile_picture_placeholder.jpg";
+import detailPost_img_placeholder from "../../assets/img/placeholders/detailPost_placeholder.png";
+import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
+
+import {
+    Typography,
+    makeStyles,
+    Box,
+    Avatar,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Slide,
+    Dialog,
+    Button,
+    Chip
+  } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     appBar: {
-      position: 'relative',
+        position: 'relative',
     },
     title: {
-      marginLeft: theme.spacing(2),
-      flex: 1,
+        marginLeft: theme.spacing(2),
+        flex: 1,
     },
-}));
+    headerDetail: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        marginTop: '10%'
+    },
+    profilePicture: {
+        width: theme.spacing(8),
+        height: theme.spacing(8),      
+    },
+    opdracht: {
+        marginLeft: theme.spacing(),    
+    },
+    opdrachtSubtitle: {
+        opacity: 0.6,
+    },
+    detailContainer: {
+        width: '87%',
+        margin: '0 auto',
+    },
+    detailsOpdracht: {
+        opacity: 0.7,
+        fontSize: theme.spacing(1.7),
+        marginTop: '5%'
+    },
+    lakenhalLogo: {
+        width: theme.spacing(5),
+        height: theme.spacing(6),
+        right: '5%',
+        position: 'absolute'
+      },
+    detailsOpdrachtBottom: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginTop: '5%'
+    },
+    detailsDatumText: {
+        opacity: 0.7,
+        fontSize: theme.spacing(1.7),
+    },
+    detailsDatum:{
+        display: 'flex'
+    },
+    detailsDatumIcon: {
+        opacity: 0.7
+    }
+})); 
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
@@ -45,6 +96,31 @@ const DetailPost = ({open, closeScreen}) => {
                 </Typography>
             </Toolbar>
             </AppBar>
+            <img src={detailPost_img_placeholder}/>
+            <Box className={classes.detailContainer}>
+                <img src={lakenhal_sw} className={classes.lakenhalLogo}/>
+                <Box className={classes.headerDetail}>
+                    <Avatar
+                        alt="Profiel foto"
+                        className={classes.profilePicture}
+                        src={pf}
+                    />
+                    <Box className={classes.opdracht}>
+                        <Typography variant='h6' component='h3' className={classes.opdrachtTitle}>3D Vilten</Typography>
+                        <Typography variant='caption' className={classes.opdrachtSubtitle}>Thuisatelier opdracht</Typography>
+                    </Box>
+                </Box>
+                <Typography variant='body1' component='p' className={classes.detailsOpdracht}>
+                Vilt ontstaat als de schubben van wol aan elkaar gaan haken. Dat doe je met warm water en zeep. Door deze combinatie door je handen te wrijven, ontstaat er een stevig lapje stof. Deze stof, die heel warm, stevig en waterdicht is, wordt laken genoemd. De techniek is eeuwenoud en wordt ook vandaag de dag  nog gebruikt. Dit lijkt mij een leuke opdracht om mee bezig te zijn, maar ik zoek wat mensen om dit mee samen te doen.
+                </Typography>
+                <Box className={classes.detailsOpdrachtBottom}>
+                    <Chip label='Thuis atelier'/>
+                    <Box className={classes.detailsDatum}>
+                        <QueryBuilderIcon className={classes.detailsDatumIcon}/>
+                        <Typography className={classes.detailsDatumText} variant='subtitle1' component='p'>29 maart 2021</Typography>
+                    </Box>
+                </Box>
+            </Box>
         </Dialog>
     </div>
     )
