6a8efa49d4fd32db024afb91361639c2b98e92ac
samirtown <samirf4thi@outlook.com>
2021-06-03 19:12:45 +0200

    detailPost pop up

--- src/pages/moderatorRapportage/moderatorRapportage.jsx
+++ src/pages/moderatorRapportage/moderatorRapportage.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useAuth } from '../../contexts/authContext'
 import pf from "../../assets/img/placeholders/profile_picture_placeholder.jpg";
 import lakenhal_sw from "../../assets/img/lakenhal_sw.png";
+import { useState } from "react";
 import ModeratorRapportageCard from '../moderatorRapportage/moderatorRapportageCard';
+import DetailPost from "../../components/detailPost/detailPost";
 import isJson from '../../contexts/isJson'
 
 import {
@@ -19,6 +21,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const ModeratorRapportage = () => {
+  const [detailRapportageOpen, setDetailRapportageOpen] = useState(false)
   const classes = useStyles();
   const { currentUser } = useAuth();
 
@@ -26,29 +29,25 @@ const ModeratorRapportage = () => {
   if(isJson(currentUser)){
     user = JSON.parse(currentUser);
   }
+
+  const detailRapportageClick = () =>{
+    setDetailRapportageOpen(!detailRapportageOpen)
+  }
+
   return (
     <div>
-      {user.admin ? ( 
+      {user.admin ?  
         <Box className={classes.pageContainer}>
           <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <ModeratorRapportageCard />
-            </Grid>
-            <Grid item xs={12}>
-              <ModeratorRapportageCard />
-            </Grid>
-            <Grid item xs={12}>
-              <ModeratorRapportageCard />
-            </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={12} onClick={detailRapportageClick}>
               <ModeratorRapportageCard />
             </Grid>
           </Grid>
-        </Box>
-      )
-      : (
+        </Box>     
+      : 
         <div>404</div>
-      )}
+      }
+      <DetailPost open={detailRapportageOpen} closeScreen={detailRapportageClick}/>
     </div>
   );
 };
