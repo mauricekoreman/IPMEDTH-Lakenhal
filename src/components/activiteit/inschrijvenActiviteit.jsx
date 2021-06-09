0d0f471f08d3c9adb4bc513dc9cb205925c89b18
thimohenraat <thimohenraat@gmail.com>
2021-06-09 10:19:23 +0200

    feedback block

--- src/components/activiteit/inschrijvenActiviteit.jsx
+++ src/components/activiteit/inschrijvenActiviteit.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { TextField, Button, Box, makeStyles } from '@material-ui/core';
 import axios from "axios";
+import FeedbackBlock from "../feedbackBlock/feedbackBlock";
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -11,8 +12,8 @@ const useStyles = makeStyles((theme) => ({
 const InschrijvenActiviteit = ({user, activiteit}) => {
     const TEST_URL = "http://127.0.0.1:8000/api/";
     const classes = useStyles();
-
     const [show, setShow] = useState(false);
+    const [verstuur, setVerstuur] = useState(false);
 
     const initialValues ={
         user_ID: user.user_ID, 
@@ -39,6 +40,8 @@ const InschrijvenActiviteit = ({user, activiteit}) => {
             headers: { Accept: "application/json" },
         }).then(res => {
             console.log(res.data);
+            setShow(prev => !prev);
+            setVerstuur(true)
         })
         .catch(error => {
             console.log(error.response);
@@ -46,31 +49,32 @@ const InschrijvenActiviteit = ({user, activiteit}) => {
     }
 
     return (
-        <Box display="flex" justifyContent="center" className={classes.button}>
-            {show === false ?
-                <Button variant="contained" color="primary" onClick={() => setShow(prev => !prev)}> 
-                    Doe Mee!
-                </Button>
-            :
-                <form>
-                    <TextField
-                        label='bericht'
-                        name='bericht'
-                        value={inschrijving.inschrijving}
-                        onChange={handleInput}
-                        helperText="optioneel* Vertel waarom je mee wilt doen."
-                    />
-                    <Button variant="contained" color="primary" onClick={onSubmit}> 
-                        Verstuur
+        <div>
+            {verstuur && <FeedbackBlock success={true} text={"U bent aangemeld"} />}
+            <Box display="flex" justifyContent="center" className={classes.button}>
+                {show === false ?
+                    <Button variant="contained" color="primary" onClick={() => setShow(prev => !prev)}> 
+                        Doe Mee!
                     </Button>
-                    <Button onClick={() => setShow(prev => !prev)}> 
-                        Annuleer
-                    </Button>
-                </form>
-            }
-
-
-        </Box>
+                :
+                    <form>
+                        <TextField
+                            label='bericht'
+                            name='bericht'
+                            value={inschrijving.inschrijving}
+                            onChange={handleInput}
+                            helperText="optioneel* Vertel waarom je mee wilt doen."
+                        />
+                        <Button variant="contained" color="primary" onClick={onSubmit}> 
+                            Verstuur
+                        </Button>
+                        <Button onClick={() => setShow(prev => !prev)}> 
+                            Annuleer
+                        </Button>
+                    </form>
+                }
+            </Box>
+        </div>
     );
 };
 
