a92af84330a08f4935421dd888d4f5baface741b
Dirk Jan <dirkjanremmerswaal95@gmail.com>
2021-06-19 13:30:33 +0200

    minor fixes

--- src/components/activiteit/postList.jsx
+++ src/components/activiteit/postList.jsx
@@ -1,22 +1,15 @@
 import React, {useState, useEffect, Component} from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import axios from "axios";
-import isJson from '../../contexts/isJson'
 import FlatList from 'flatlist-react';
 import { Button } from '@material-ui/core/';
 import DetailPost from "../detailPost/detailPost";
@@ -50,20 +43,8 @@ const activiteitData = [];
  
 const PostList = ({values}) => {
     const classes = useStyles();
-    // const [values, setValues] = useState(activiteitData);
     const [detailActiviteitOpen, setDetailActiviteitOpen] = useState(false)
     const [detailActiviteit, setDetailActiviteit] = useState([])
-
-    // useEffect(() => {
-    //     axios.get(TEST_URL+'activiteitenUsers')
-    //             .then(response => {
-    //                 console.log(response.data)
-    //                 setValues(response.data)             
-    //             })
-    //             .catch(error => {
-    //                 console.log(error.response)
-    //             })
-    //   }, []);
 
     const activiteitClick = (activiteit) =>{
       setDetailActiviteitOpen(!detailActiviteitOpen)
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from "react";
 
-import Typography from "@material-ui/core/Typography";
 import CreatePost from "../../components/activiteit/createpost";
 import PostList from "../../components/activiteit/postList"
 import axios from "axios";
