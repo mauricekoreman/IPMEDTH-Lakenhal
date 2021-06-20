4ea6f1557dd8dab6e6a0ebb7e260f623f90bdf5a
thimohenraat <thimohenraat@gmail.com>
2021-06-20 19:22:39 +0200

    test change

--- client/src/test/login.test.jsx
+++ client/src/test/login.test.jsx
@@ -4,13 +4,12 @@ import { render, createShallow, act, screen} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Login from '../components/authentication/loginForm'
 
-describe("account delete form", () => {
+describe("login form", () => {
+    const { getByTestId } = render(<Login />);
+    const password = getByTestId("password") 
+    const email = getByTestId("email");
+    
     it("renders default state", () => {
-      const { getByTestId } = render(<Login />);
-  
-      const password = getByTestId("password") 
-      const email = getByTestId("email");
- 
       expect(password.value).toBe("");
       expect(email.value).toBe("");
     });
